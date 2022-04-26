14544556e5f1c8f67c6cee4f401eff1d0ff0eea9
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2022-04-26 20:57:45 +0530

    plan_apply

--- src/mytrackfone/devops/plan_apply/Plan.groovy
+++ src/mytrackfone/devops/plan_apply/Plan.groovy
@@ -6,7 +6,7 @@ def plan(mytool,var_file,my_region){
     withAWS(credentials: 'my-aws-cred', region: "${my_region}") {
               sh """                  
                  ${mytool}/terraform init
-                 ${mytool}/terraform plan -var-file=${var_file} -out=planfile"
+                 ${mytool}/terraform plan -var-file=${var_file} -out=planfile
               """
          }
 
