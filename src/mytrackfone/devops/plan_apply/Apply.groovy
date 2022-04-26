1477ade356aa1bd47179337893f5766ec27929fc
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2022-04-26 20:59:48 +0530

    plan_apply

--- src/mytrackfone/devops/plan_apply/Apply.groovy
+++ src/mytrackfone/devops/plan_apply/Apply.groovy
@@ -5,10 +5,10 @@ def apply(mytool,var_file,my_region){
 
     withAWS(credentials: 'my-aws-cred', region: "${my_region}") {
               sh """                  
-                 ${mytool}/terraform apply -var-file=${var_file} -auto-approve"
+                 ${mytool}/terraform apply -var-file=${var_file} -auto-approve
               """
 
-              
+
          }
 
 }
--- src/mytrackfone/devops/plan_apply/Destroy.groovy
+++ src/mytrackfone/devops/plan_apply/Destroy.groovy
@@ -4,7 +4,7 @@ def destroy(mytool,var_file,my_region){
 
     withAWS(credentials: 'my-aws-cred', region: "${my_region}") {
               sh """                  
-                 ${mytool}/terraform destroy -var-file=${var_file} -auto-approve"
+                 ${mytool}/terraform destroy -var-file=${var_file} -auto-approve
               """
 
               
